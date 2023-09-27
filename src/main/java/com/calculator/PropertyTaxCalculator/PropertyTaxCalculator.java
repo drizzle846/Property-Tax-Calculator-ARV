093a1d1f0458a9f65715a59108994e13e00eae51
drizzle846 <125164693+drizzle846@users.noreply.github.com>
2023-09-26 23:43:44 -0700

    Add files via upload

---
+++ src/main/java/com/calculator/PropertyTaxCalculator/PropertyTaxCalculator.java
@@ -0,0 +1,8 @@
+package com.calculator.PropertyTaxCalculator;
+
+public class PropertyTaxCalculator {
+    public static double calculateProprtyTax(double AVR, double taxRate) {
+        return AVR* taxRate;
+    }}
+
+    
